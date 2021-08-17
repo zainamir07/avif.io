42008b40417107b12b36f17896eac9e0b62a4976
Justin Schmitz <justin.schmitz97@gmail.com>
2021-08-17 14:16:41 +0200

    Sharp setting changes

--- utils/sharp.js
+++ utils/sharp.js
@@ -7,8 +7,8 @@ const path = require("path");
 const output = "../public/img/";
 const input = "../images/";
 const jpgQuality = { mozjpeg: true, quality: 50, progressive: true };
-const webpQuality = { quality: 45, reductionEffort: 3 };
-const avifQuality = { quality: 40, speed: 3 };
+const webpQuality = { quality: 48, reductionEffort: 4 };
+const avifQuality = { quality: 55, speed: 2 };
 const sizes = [1536, 768, 576, 384];
 fs.readdir(input, (err, files) => {
   console.log(
