535b47a78ac0ba6d3d3d881a1728e370c917ed97
Muhammad Rivki <muhammad.rivki@live.com>
2021-08-18 23:19:43 +0700

    feat: move avif vs bmp to mdx (#2)

--- components/Blog/Posts.tsx
+++ components/Blog/Posts.tsx
@@ -3,19 +3,21 @@ import Link from "next/link";
 export default function BlogPosts(props: any) {
   const posts = props.posts;
 
-  const listItems = posts.map(({data, slug}: any, index: any) => (
-    <Link href={`/${props.title}/${slug}`} key={index}>
+  console.log(posts)
+
+  const listItems = posts.map(({data, slug, keyword, description, category}: any, index: any) => (
+    <Link href={`/blog/${props.title || category}/${slug}`} key={index}>
       <a
-        href={`/${props.title}/${slug}`}
+        href={`/blog/${props.title || category}/${slug}`}
         tabIndex={0}
         className="p-2 cursor-pointer md:w-1/2 lg:w-1/3 group"
       >
-        <div className="overflow-hidden h-full rounded-md bg-bg-300">
+        <div className="h-full overflow-hidden rounded-md bg-bg-300">
           <div className="p-3 bg-bg-500 group-hover:bg-gradient">
-            <div className="font-bold text-white">#{data?.keyword || ''}</div>
+            <div className="font-bold text-white">#{data?.keyword || keyword || ''}</div>
           </div>
           <div className="p-3 border-t-2 group-hover:text-white border-bg-700">
-            <p>{data?.description || ''}</p>
+            <p>{data?.description || description || ''}</p>
           </div>
         </div>
       </a>
