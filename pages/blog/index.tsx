266a65c3085a64f5b2818864ad6d04c20f41322c
Muhammad Rivki <Muhammad.Rivki@live.com>
2021-09-02 09:15:58 +0700

    feat: add filter by categorie or subcategories

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -3,6 +3,7 @@ import path from "path";
 import * as React from "react";
 import { InferGetStaticPropsType, NextPage } from "next";
 import matter from "gray-matter";
+import clsx from "clsx";
 
 import { postFilePaths, BLOG_POSTS_PATH } from "@utils/mdx";
 import Posts from "@components/Blog/Posts";
@@ -53,15 +54,38 @@ export const getStaticProps = async () => {
       defaultFilteredPost,
       listSubCategories,
       listCategories,
+      listAllCategories: [...listCategories, ...listSubCategories],
       posts: listPostsByFolder as any,
     },
   };
 };
 
 type PostsPageProps = InferGetStaticPropsType<typeof getStaticProps>;
-const BlogAvif: NextPage<PostsPageProps> = ({ defaultFilteredPost, posts }) => {
+const BlogAvif: NextPage<PostsPageProps> = ({
+  defaultFilteredPost,
+  posts,
+  listAllCategories,
+}) => {
   const [filteredPost, setFilteredPost] = React.useState([]);
   const [filterKeyword, setFilterKeyword] = React.useState("");
+  const [selectedCategoryPill, setSelectedCategoryPill] = React.useState("");
+
+  const handleSelectedPill = (category: string) => {
+    if (category === selectedCategoryPill) {
+      setSelectedCategoryPill("");
+      setFilteredPost([]);
+      return;
+    }
+
+    setSelectedCategoryPill(category);
+    const filteredPosts = defaultFilteredPost.filter((post) => {
+      return (
+        post.data.category === category || post.data.subcategory === category
+      );
+    });
+
+    setFilteredPost(filteredPosts as any);
+  };
 
   const handleFilterByKeyword = (
     event: React.ChangeEvent<HTMLInputElement>
@@ -81,15 +105,61 @@ const BlogAvif: NextPage<PostsPageProps> = ({ defaultFilteredPost, posts }) => {
           <h1>{meta.blog.title}</h1>
           <h2 className="text-base">{meta.blog.description}</h2>
         </div>
-        <div>
-          <input
-            type="text"
-            placeholder="search"
-            onChange={handleFilterByKeyword}
-          />
-        </div>
         <div className="container max-w-screen-lg">
-          {filterKeyword.length > 0 ? (
+          <div className="relative mt-1 mb-2 rounded-md shadow-sm">
+            <input
+              type="text"
+              placeholder="Search Articles"
+              className="block py-1 px-3 pr-10 w-full text-gray-800 rounded-md outline-none focus:ring-2 focus:ring-red-600 focus:ring-offset-4 focus:ring-offset-bg-700"
+              onChange={handleFilterByKeyword}
+            />
+            <div className="flex absolute inset-y-0 right-0 items-center pr-3 pointer-events-none">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-4 h-4 text-gray-800"
+                aria-hidden="true"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </div>
+          </div>
+          <div>
+            {listAllCategories.map((category) => (
+              <button
+                key={category}
+                onClick={() => handleSelectedPill(category)}
+                className={clsx(
+                  "inline-flex items-center px-3 py-0 mt-2 mr-2 py-0.5 rounded-full text-sm font-medium cursor-pointer bg-red-100 text-red-800",
+                  selectedCategoryPill === category &&
+                    "bg-indigo-600 border-transparent text-white hover:bg-indigo-700"
+                )}
+              >
+                {selectedCategoryPill === category && (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="mr-2 -ml-1 w-3 h-3"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                )}
+                <p>{category}</p>
+              </button>
+            ))}
+          </div>
+
+          {filterKeyword.length > 0 || filteredPost.length ? (
             <div className="grid grid-cols-1 gap-4 m-2 md:grid-cols-2 md:m-3 lg:grid-cols-3">
               {filteredPost.map((post: any) => (
                 <Post key={post.slug} {...post.data} slug={post.slug} />
