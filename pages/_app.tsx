8bef5092db6c6ab3661c6f6493145e350dc14de7
ADESSO\juschmitz <justin.schmitz@adesso.de>
2021-09-05 00:03:56 +0200

    Firebase Modular Approach

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,22 +2,33 @@ import "@styles/global.css";
 import { useEffect } from "react";
 import Script from "next/script";
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAnyQzUmFahQiH8XBsJfCJY8ogZvUT8o8E",
+  authDomain: "avif-a6e25.firebaseapp.com",
+  databaseURL: "https://avif-a6e25.firebaseio.com",
+  projectId: "avif-a6e25",
+  storageBucket: "avif-a6e25.appspot.com",
+  messagingSenderId: "1077707178739",
+  appId: "1:1077707178739:web:c98c39aae900425defad86",
+  measurementId: "G-Z25VRG05C4",
+};
+
 export default function AvifIo({ Component, pageProps }: any) {
   useEffect(arrayBufferPolyfill, []);
+
+  useEffect(() => {
+    if (process.env.NODE_ENV === "production") {
+      const app = initializeApp(firebaseConfig);
+      const analytics = getAnalytics(app);
+    }
+  }, []);
+
   return (
     <>
       <Script strategy="beforeInteractive" src="/avif.js" />
-      {/*<Script src="/hotjar.js" /> Disabled for now*/}
-      <Script
-        strategy="beforeInteractive"
-        src="/__/firebase/8.10.0/firebase-app.js"
-      />
-      <Script strategy="beforeInteractive" src="/__/firebase/init.js" />
-      <Script
-        strategy="beforeInteractive"
-        src="/__/firebase/8.10.0/firebase-analytics.js"
-      />
-      <Script id="analytics">firebase.analytics()</Script>
       <div className="overflow-x-hidden page">
         <Component {...pageProps} />
       </div>
