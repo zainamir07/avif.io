6585a1bb21329e5d29e4287aa3ec2794ed4fe2bf
justinschmitz97 <justin.schmitz97@gmail.com>
2021-10-20 17:27:29 +0200

    Support Bar over Header

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -3,7 +3,6 @@ import NextLink from "next/link";
 import React, { useEffect, useState } from "react";
 
 export default function Header() {
-  /* Detect scroll for fixed header */
   const [isFixed, setIsFixed] = useState(true);
   const [support, setSupport] = useState(false);
   const handleScroll = () => {
@@ -44,8 +43,8 @@ export default function Header() {
         } ${support ? "bg-gradient" : "bg-bg-200"}`}
       >
         {support
-          ? "Your browser supports AVIF."
-          : "Your browser does not support AVIF."}
+          ? "Your browser supports AVIF.🥳"
+          : "Your browser does not support AVIF.😞"}
       </div>
       <div className="py-1 px-3 flex flex-wrap justify-between items-center md:flex-row">
         <nav className="flex flex-wrap justify-center items-center text-base md:pl-4 md:mr-auto">
@@ -70,12 +69,6 @@ export default function Header() {
             text="Github"
             href="github.com/justinschmitz97/avif.io/"
           />
-          {/*
-          <Link
-            className="hidden p-2 md:block md:p-3"
-            text="JXL Converter"
-            href="jpegxl.io"
-    />*/}
         </nav>
         <NextLink href="/">
           <button className="inline-flex items-center py-1 px-2 text-base rounded border-0 md:mt-0 focus:outline-none bg-bg-200 hover:bg-bg-300">
