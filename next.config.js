9915546f95cc526d4b730886c5f8a885b20e7f02
Justin Schmitz <justin.schmitz97@gmail.com>
2022-04-17 10:41:57 +0200

    .

--- next.config.js
+++ next.config.js
@@ -1,31 +1,10 @@
-const withMDX = require("@next/mdx")({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [],
-    providerImportSource: "@mdx-js/react",
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  eslint: {
+    ignoreDuringBuilds: true,
   },
-});
+};
 
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
-
-module.exports = withBundleAnalyzer(
-  withMDX({
-    swcMinify: true,
-    reactStrictMode: true,
-    webpack: (config, { dev, isServer }) => {
-      // Replace React with Preact only in client production build
-      if (!dev && !isServer) {
-        Object.assign(config.resolve.alias, {
-          "react/jsx-runtime.js": "preact/compat/jsx-runtime",
-          react: "preact/compat",
-          "react-dom/test-utils": "preact/test-utils",
-          "react-dom": "preact/compat",
-        });
-      }
-      return config;
-    },
-  })
-);
+module.exports = nextConfig;
