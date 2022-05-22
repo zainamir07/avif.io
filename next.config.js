3fc1af011ce4ace25e7fbb5ef7bce84ad4ffa945
Justin Schmitz <justin.schmitz97@gmail.com>
2022-05-22 08:54:18 +0200

    Next.Config

--- next.config.js
+++ next.config.js
@@ -4,24 +4,7 @@ const { withContentlayer } = require("next-contentlayer");
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   trailingSlash: true,
-  webpack: (config, { dev, isServer }) => {
-    // Replace React with Preact only in client production build
-
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        "react/jsx-runtime.js": "preact/compat/jsx-runtime",
-        react: "preact/compat",
-        "react-dom/test-utils": "preact/test-utils",
-        "react-dom": "preact/compat",
-      });
-    }
-
-    return config;
-  },
 };
 
 module.exports = withContentlayer(nextConfig);
