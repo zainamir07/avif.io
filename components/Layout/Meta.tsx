80012210625c7909895e2f1155a5dc5886eba7a2
Justin Schmitz <justin.schmitz97@gmail.com>
2022-05-29 12:19:18 +0200

    canonical fix

--- components/Layout/Meta.tsx
+++ components/Layout/Meta.tsx
@@ -17,7 +17,7 @@ export default function Meta(props: Props) {
     <Head>
       <link
         rel="canonical"
-        href={`${process.env.NEXT_PUBLIC_SITE_URL}${url}`}
+        href={`${process.env.NEXT_PUBLIC_SITE_URL}${url}/`}
       />
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
