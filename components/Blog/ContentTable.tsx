48c90e83e99ca601329ecbb752c195e16c605368
Justin Schmitz <justin.schmitz97@gmail.com>
2022-06-18 00:31:12 +0200

    fix table of content visual overlapping

--- components/Blog/ContentTable.tsx
+++ components/Blog/ContentTable.tsx
@@ -11,7 +11,7 @@ export interface ContentTableProps {
 
 export default function ContentTable(props: ContentTableProps) {
   return (
-    <nav className="p-2 mt-4 mb-2 rounded-md md:p-4 2xl:absolute 2xl:left-0 2xl:mt-0 2xl:rounded-l-none bg-bg-400">
+    <nav className="p-2 mt-4 mb-2 rounded-md md:p-4 3xl:absolute 3xl:left-0 3xl:mt-0 3xl:rounded-l-none bg-bg-400">
       <h4 className="m-0 bold">Table of Content</h4>
       <ol className="m-0">
         {props.contentTable.map((entry, index: any) => (
